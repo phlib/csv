aeabe04518f689f967f5b32edb80f72cb3c24802
Chris Minett <chris.minett@emailcenteruk.com>
2018-06-27 13:19:34 +0100

    Add memory tests to Factory for using large files
    Memory shouldn't be a worry for direct CSV files, but zipped files are re-written to a
    new stream, so it's important to test that large files won't cause issues.

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -55,4 +55,60 @@ class FactoryTest extends TestCase
         $filename = __DIR__ . '/_files/does-not-exist.csv.zip';
         Factory::createFromZipFile($filename);
     }
+
+    public function testCreateFromLargeFileIsMemoryEfficient()
+    {
+        if (isset($GLOBALS['SKIP_LARGE'])) {
+            $this->markTestSkipped('Large test skipped; SKIP_LARGE is true');
+            return;
+        }
+
+        $filename = __DIR__ . '/_files/large.csv';
+        $fp = fopen($filename, 'w+b');
+        $size = 100 * 1024 * 1024; // 100MiB file
+
+        while ($size > 0) {
+            $size -= fwrite($fp, random_bytes(8192));
+        }
+        fclose($fp);
+
+        try {
+            Factory::createFromFile($filename);
+        } finally {
+            unlink($filename);
+            $this->assertLessThan(20 * 1024 * 1024, memory_get_peak_usage()); // less than 20MiB of memory was used
+        }
+    }
+
+    public function testCreateFromLargeZipFileIsMemoryEfficient()
+    {
+        if (isset($GLOBALS['SKIP_LARGE'])) {
+            $this->markTestSkipped('Large test skipped; SKIP_LARGE is true');
+            return;
+        }
+
+        $filename = __DIR__ . '/_files/large.csv';
+        $fp = fopen($filename, 'w+b');
+        $size = 100 * 1024 * 1024; // 100MiB file
+
+        while ($size > 0) {
+            $size -= fwrite($fp, random_bytes(8192));
+        }
+        fclose($fp);
+
+        $zipName = __DIR__ . '/_files/large.csv.zip';
+        $zip = new \ZipArchive();
+        $zip->open($zipName, \ZipArchive::CREATE);
+        $zip->addFile($filename);
+        $zip->close();
+
+        unlink($filename);
+
+        try {
+            Factory::createFromZipFile($zipName);
+        } finally {
+            unlink($zipName);
+            $this->assertLessThan(20 * 1024 * 1024, memory_get_peak_usage()); // less than 20MiB of memory was used
+        }
+    }
 }
